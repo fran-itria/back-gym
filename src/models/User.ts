a1bc5874b01cf2ec49eea774434dc16732ec537a
Franco Itria <francoitria01@gmail.com>
2023-11-24 19:07:11 -0300

    Add propertys

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import { DataType, DataTypes, Sequelize } from "sequelize";
+import { DataTypes, Sequelize } from "sequelize";
 
 export default function (sequelize: Sequelize) {
     sequelize.define(
@@ -9,6 +9,10 @@ export default function (sequelize: Sequelize) {
                 primaryKey: true,
                 defaultValue: DataTypes.UUIDV4
             },
+            gym: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
             name: {
                 type: DataTypes.STRING,
                 allowNull: false
@@ -16,6 +20,38 @@ export default function (sequelize: Sequelize) {
             surname: {
                 type: DataTypes.STRING,
                 allowNull: false
+            },
+            dni: {
+                type: DataTypes.NUMBER,
+                allowNull: false
+            },
+            edad: {
+                type: DataTypes.NUMBER,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            usuario: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            phone: {
+                type: DataTypes.NUMBER,
+                allowNull: false
+            },
+            contactEmergency: {
+                type: DataTypes.NUMBER,
+                allowNull: false
+            },
+            admin: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            pay: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
             }
         },
         {
