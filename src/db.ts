fec454856987d4e7fd68fc2838db0ddbcf56880d
Franco Itria <francoitria01@gmail.com>
2023-11-24 19:06:42 -0300

    Delete comments

--- src/db.ts
+++ src/db.ts
@@ -8,8 +8,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const sequelize = new Sequelize(
     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/gym`,
     {
-        logging: false, // set to console.log to see the raw SQL queries
-        native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+        logging: false,
+        native: false,
     }
 );
 
@@ -18,5 +18,5 @@ user(sequelize)
 const { User } = sequelize.models
 
 
-export { User } /// para poder importar los modelos así: const { Product, User } = require('./db.js');
-export const conn = sequelize // para importart la conexión { conn } = require('./db.js');
+export { User }
+export const conn = sequelize
