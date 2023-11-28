7a5be0ad1387470c806e1fe221feac263126a6d8
Franco Itria <francoitria01@gmail.com>
2023-11-28 19:13:59 -0300

    Change in routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,21 @@
 import { Router, Request, Response } from "express";
 import { error } from "console";
-import { Gym, User } from "../db";
+import { Gym, Rutina, User } from "../db";
 
 const user = Router()
 
 user.get('', async (_req: Request, res: Response) => {
     try {
-        const users = await User.findAll()
+        const users = await User.findAll({
+            include: [{
+                model: Gym,
+                attributes: ["name"]
+            },
+            {
+                model: Rutina
+            }
+            ]
+        })
         res.status(200).json(users)
     } catch (error) {
         console.log(error)
@@ -18,14 +27,13 @@ user.post('', async (req: Request, res: Response) => {
     const { gymName, name, surname, contactEmergency, dni, edad, email, phone, usuario } = req.body
     try {
         const gym: any = await Gym.findOne({ where: { name: gymName } })
-        console.log(gym)
         if (!gym) throw error('Gimnasio no encontrado')
         const newUser = await User.create({ name, surname, contactEmergency, dni, edad, email, phone, usuario })
         await gym.addUser(newUser)
         res.status(200).json(newUser)
     } catch (error) {
         console.log(error)
-        res.status(404).json(error)
+        res.status(404).json({ Errors: error })
     }
 })
 
