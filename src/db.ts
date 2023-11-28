ca41b3b316c8bb6284f268eb78f528dfac51e696
Franco Itria <francoitria01@gmail.com>
2023-11-28 19:13:01 -0300

    New relations

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,9 @@ require('dotenv').config();
 import { Sequelize } from 'sequelize'
 import user from "./models/User";
 import gym from './models/Gym'
+import rutina from './models/Rutina';
+import day from './models/Day';
+import ejercicio from './models/Ejercicio';
 
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
@@ -16,12 +19,26 @@ const sequelize = new Sequelize(
 
 user(sequelize)
 gym(sequelize)
+rutina(sequelize)
+day(sequelize)
+ejercicio(sequelize)
 
-const { User, Gym } = sequelize.models
+const { User, Gym, Rutina, Day, Ejercicio } = sequelize.models
 
 // GYM - USER
 Gym.hasMany(User, { foreignKey: 'GymId' })
 User.belongsTo(Gym, { foreignKey: 'GymId' })
+// USER - RUTINA
+User.hasMany(Rutina, { foreignKey: 'UserId' })
+Rutina.belongsTo(User, { foreignKey: 'UserId' })
+// RUTINA - DAY
+Rutina.hasMany(Day, { foreignKey: 'RutinaId' })
+Day.belongsTo(Rutina, { foreignKey: 'RutinaId' })
+// DAY - EJERCICIO
+Day.hasMany(Ejercicio, { foreignKey: 'DayId' })
+Ejercicio.belongsTo(Day, { foreignKey: 'DayId' })
 
-export { User, Gym }
+
+
+export { User, Gym, Rutina, Day, Ejercicio }
 export const conn = sequelize
