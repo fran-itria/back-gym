056d47ab7bbbcdf7b0ea04853fe042cc227362e7
fran-itria <francoitria01@gmail.com>
2023-11-29 15:58:39 -0300

    Change name type

--- src/handlers/user/createUser.ts
+++ src/handlers/user/createUser.ts
@@ -1,11 +1,30 @@
 import { User } from "../../db";
-import { userPrpertys } from "../../types";
+import { userPropertys } from "../../types";
 import getOneGymName from "../gym/getOneGymName";
 
-export default async function createUser(propertys: userPrpertys) {
-    const { gymName, name, surname, contactEmergency, dni, edad, email, phone, usuario } = propertys
-    const gym: any = await getOneGymName(gymName)
-    const newUser = await User.create({ name, surname, contactEmergency, dni, edad, email, phone, usuario })
-    await gym.addUser(newUser)
-    return newUser
+export default async function createUser(propertys: userPropertys) {
+  const {
+    gymName,
+    name,
+    surname,
+    contactEmergency,
+    dni,
+    edad,
+    email,
+    phone,
+    usuario,
+  } = propertys;
+  const gym: any = await getOneGymName(gymName);
+  const newUser = await User.create({
+    name,
+    surname,
+    contactEmergency,
+    dni,
+    edad,
+    email,
+    phone,
+    usuario,
+  });
+  await gym.addUser(newUser);
+  return newUser;
 }
