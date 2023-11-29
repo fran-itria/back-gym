8b2575bc408313f38638740e58c9f56fabb33d74
fran-itria <francoitria01@gmail.com>
2023-11-29 16:18:09 -0300

    get and cerate rutina controller

--- src/routes/rutina.ts
+++ src/routes/rutina.ts
@@ -1,31 +1,10 @@
-import { Router, Request, Response } from "express";
-import { Day, Ejercicio, Rutina, User } from "../db";
-import { error } from "console";
+import { Router } from "express";
 import getRutinasController from "../controllers/rutina/getRutinas";
+import createRutinaController from "../controllers/rutina/createRutina";
 
-const rutina = Router()
+const rutina = Router();
 
-rutina.get('', getRutinasController)
+rutina.get("", getRutinasController);
+rutina.post("", createRutinaController);
 
-rutina.post('', async (req: Request, res: Response) => {
-    const { userId, series, repeticiones, ejercicioName } = req.body
-    try {
-        const user: any = await User.findByPk(userId)
-        if (!user) throw error('Usuario no encontrado')
-        const day: any = await Day.create()
-        console.log(day)
-        const ejercicio: any = await Ejercicio.create({ series, repeticiones, name: ejercicioName })
-        console.log(ejercicio)
-        await day.addEjercicio(ejercicio)
-        const newRutina: any = await Rutina.create()
-        await newRutina.addDay(day)
-        await user.addRutina(newRutina)
-        res.status(200).json(day)
-    } catch (error: any) {
-        console.log(error)
-        res.status(404).json(error)
-    }
-})
-
-
-export default rutina
+export default rutina;
