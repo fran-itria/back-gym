a1a934f7ca40b97904389970848aca78a62133ac
Franco Itria <francoitria01@gmail.com>
2023-12-11 04:23:16 -0300

    Corrected add first cargas when cargas is null

--- src/handlers/ejercicio/updateEjercicio.ts
+++ src/handlers/ejercicio/updateEjercicio.ts
@@ -5,13 +5,23 @@ export const updateEjercicio = async (changes: changeEjercicioPropertys) => {
     const { id, cargas, name, repeticiones, series } = changes
     const exercise: any = await Ejercicio.findByPk(id)
     if (!exercise) throw new Error('Ejercicio no encontrado')
-    const updateCargas = exercise.cargas.concat(cargas)
-    await exercise.update({
-        cargas: updateCargas,
-        name,
-        repeticiones,
-        series
-    })
+    if (exercise.cargas != null) {
+        const updateCargas = exercise.cargas.concat(cargas)
+        await exercise.update({
+            cargas: updateCargas,
+            name,
+            repeticiones,
+            series
+        })
+    } else {
+        const firstCargas = [cargas]
+        await exercise.update({
+            cargas: firstCargas,
+            name,
+            repeticiones,
+            series
+        })
+    }
 
     return exercise
 }
