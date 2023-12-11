4fb8c2441e1dfd27130d78705371e588302eb426
Franco Itria <francoitria01@gmail.com>
2023-12-11 01:25:33 -0300

    Correct order of days and exercises

--- src/handlers/ejercicio/createEjercicios.ts
+++ src/handlers/ejercicio/createEjercicios.ts
@@ -2,7 +2,7 @@ import { Ejercicio } from "../../db"
 import { ejerciciosPropertys } from "../../types"
 
 export const createEjercicios = async (ejercicios: ejerciciosPropertys) => {
-    const moreEjercicios = await Ejercicio.bulkCreate(ejercicios)
+    const moreEjercicios = await Ejercicio.bulkCreate(ejercicios.reverse())
     if (!moreEjercicios) throw new Error('No se pudieron crear los ejercicios')
     return moreEjercicios
 }
