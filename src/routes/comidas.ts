fec889089e86feb18c35f14a60e3f7b4e1bf003b
Franco Itria <francoitria01@gmail.com>
2023-12-14 21:39:18 -0300

    new routes

--- src/routes/comidas.ts
+++ src/routes/comidas.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import getAllMealsController from "../controllers/comidas/getMeals";
 import getOneMealController from "../controllers/comidas/getOneMeal";
 import createNewMealController from "../controllers/comidas/createNewMeal";
+import updateMealController from "../controllers/comidas/updateMeal";
+import deleteMealController from "../controllers/comidas/deleteMeal";
 
 const comidas = Router()
 
 comidas.get('', getAllMealsController)
 comidas.get('/oneMeal', getOneMealController)
 comidas.post('', createNewMealController)
+comidas.put('', updateMealController)
+comidas.delete('', deleteMealController)
 
 export default comidas
