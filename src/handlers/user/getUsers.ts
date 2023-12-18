8979d1060e85471f19a062f001a66cf8817349f0
Franco Itria <francoitria01@gmail.com>
2023-12-18 00:53:01 -0300

    Delete and include model

--- src/handlers/user/getUsers.ts
+++ src/handlers/user/getUsers.ts
@@ -1,4 +1,4 @@
-import { Calentamiento, Comidas, Day, Ejercicio, Gym, Rutina, User } from "../../db"
+import { Calentamiento, Comidas, EjercicioExtra, Gym, Rutina, User } from "../../db"
 
 const getUsers = async () => {
     const users = await User.findAll({
@@ -9,34 +9,15 @@ const getUsers = async () => {
             },
             {
                 model: Rutina,
-                include: [
-                    {
-                        model: Day,
-                        include: [
-                            {
-                                model: Ejercicio
-                            },
-                        ],
-                    }
-                ],
-
             },
             {
                 model: Calentamiento,
-                include: [
-                    {
-                        model: Day,
-                        include: [
-                            {
-                                model: Ejercicio
-                            },
-                        ],
-                    }
-                ],
-
             },
             {
                 model: Comidas
+            },
+            {
+                model: EjercicioExtra
             }
         ]
     })
