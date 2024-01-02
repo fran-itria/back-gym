f98c2464994d0bd063169073d9831e95834ec054
Franco Itria <francoitria01@gmail.com>
2024-01-02 14:17:53 -0300

    Delete error

--- src/handlers/comidas/createNewMeal.ts
+++ src/handlers/comidas/createNewMeal.ts
@@ -5,7 +5,6 @@ import { getOneUserId } from "../user/getOneUserId"
 export const createNewMeal = async (props: createMeal) => {
     const { userId, date, hour, moment, food } = props
     const user: any = await getOneUserId(userId)
-    if (!user) throw new Error('Usuario no encontrado')
     const newDate = new Date(date)
     const newComida = await Meals.create({
         date: newDate,
