62b7f56f3bcae78e6e4b00517fadaeabf9fb9751
Franco Itria <francoitria01@gmail.com>
2024-01-10 14:24:57 -0300

    exercises.reverse()

--- src/handlers/ejercicio/createExercises.ts
+++ src/handlers/ejercicio/createExercises.ts
@@ -2,7 +2,7 @@ import { Exercise } from "../../db"
 import { exercisesPropertys } from "../../types"
 
 export const createExercises = async (exercises: exercisesPropertys) => {
-    const moreExercises = await Exercise.bulkCreate(exercises)
+    const moreExercises = await Exercise.bulkCreate(exercises.reverse())
     if (!moreExercises) throw new Error('No se pudieron crear los ejercicios')
     return moreExercises
 }
