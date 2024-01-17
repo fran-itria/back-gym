c66d9238ff8f9cb2f32f8b70e590cf55c1007b77
Franco Itria <francoitria01@gmail.com>
2024-01-17 17:50:07 -0300

    Delete include model user

--- src/handlers/gym/getGyms.ts
+++ src/handlers/gym/getGyms.ts
@@ -1,12 +1,7 @@
 import { Gym, User } from "../../db"
 
 export default async function getGyms() {
-    const gyms = await Gym.findAll({
-        include: [{
-            model: User,
-            attributes: ['name', 'surname', 'usuario']
-        }]
-    })
+    const gyms = await Gym.findAll()
     if (gyms.length == 0) throw Error('No hay gimnasios')
     return gyms
 }
