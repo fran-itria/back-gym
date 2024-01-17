61f50766f1b8d340f99a559d96139d9f86bab7e0
Franco Itria <francoitria01@gmail.com>
2024-01-17 19:18:49 -0300

    Delete console log

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -6,14 +6,12 @@ export default async function createUserController(req: request, res: response) 
         const newUser = await createUser(req.body)
         if (newUser) res.status(200).json(newUser)
     } catch (error) {
-        console.log(error)
         if (error.message.includes('Users_user')) {
             res.status(404).json({ Error: "Ya existe ese usuario" })
         }
         if (error.message.includes('Users_email')) {
             res.status(404).json({ Error: "Email ya registrado" })
-        } else {
-            res.status(404).json({ Error: error.message })
         }
+        res.status(404).json({ Error: error.message })
     }
 }
