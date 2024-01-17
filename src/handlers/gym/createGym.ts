0459900b4bdd68fb0ea9eaf7d5f4d95d363eacd2
Franco Itria <francoitria01@gmail.com>
2024-01-17 19:18:37 -0300

    Delete condition

--- src/handlers/gym/createGym.ts
+++ src/handlers/gym/createGym.ts
@@ -2,6 +2,5 @@ import { Gym } from "../../db";
 
 export default async function createGym(name: string) {
     const newGym = await Gym.create({ name })
-    if (!newGym) throw new Error('No se pudo crear el gimnasio')
     return newGym
 }
