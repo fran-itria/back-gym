08ee2d16419a54ef395ccc8682927948dcefae61
Franco Itria <francoitria01@gmail.com>
2024-01-27 16:29:17 -0300

    Change default value login

--- src/models/User.ts
+++ src/models/User.ts
@@ -61,7 +61,7 @@ export default function (sequelize: Sequelize) {
             },
             login: {
                 type: DataTypes.BOOLEAN,
-                defaultValue: false
+                defaultValue: true
             }
         },
         {
