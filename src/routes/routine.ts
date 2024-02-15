51ddcb75a78b0272cd7899ab9596fe71c204b6de
Franco Itria <francoitria01@gmail.com>
2024-02-15 04:37:13 -0300

    new route fot update routine

--- src/routes/routine.ts
+++ src/routes/routine.ts
@@ -4,6 +4,7 @@ import getOneRoutineController from "../controllers/rutina/getOneRutina";
 import createOneDayRoutineController from "../controllers/rutina/createOneDayRutina";
 import createRoutineController from "../controllers/rutina/createRutina";
 import deleteRoutineController from "../controllers/rutina/deleteRutina";
+import updateRutinaController from "../controllers/rutina/updateRutina";
 
 const routine = Router();
 
@@ -11,6 +12,7 @@ routine.get("", getRoutineController);
 routine.get("/:id", getOneRoutineController)
 routine.post("/createOneDayRutina", createOneDayRoutineController);
 routine.post("/createRutina", createRoutineController);
+routine.put("", updateRutinaController)
 routine.delete("", deleteRoutineController)
 
 export default routine;
