af2ca6d8a330d7efe3758a25962e4e1e59fbaf2f
Franco Itria <francoitria01@gmail.com>
2024-02-18 15:47:55 -0300

    Ordered routine by day

--- src/handlers/rutina/getOneRoutine.ts
+++ src/handlers/rutina/getOneRoutine.ts
@@ -14,9 +14,10 @@ export default async function getOneRoutine(id: string) {
                     ]
                 },
             ],
-            attributes: ['id', 'WarmUpId']
+            attributes: ['id', 'WarmUpId', 'numberDay']
         }
     })
+    routine.Days.sort((day1: { numberDay: number; }, day2: { numberDay: number; }) => day1.numberDay - day2.numberDay)
     routine.Days.forEach((day: { Exercises: { exercise: number; }[]; }) => {
         day.Exercises.sort((exercise1: { exercise: number; }, exercise2: { exercise: number; }) => {
             if (exercise1.exercise && exercise2.exercise) return exercise1.exercise - exercise2.exercise
