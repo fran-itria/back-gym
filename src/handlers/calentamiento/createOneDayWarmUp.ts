2dde7ae133ca1d624cd6404eb6755904d4a1edc8
Franco Itria <francoitria01@gmail.com>
2024-02-24 03:34:32 -0300

    Return warm up complete

--- src/handlers/calentamiento/createOneDayWarmUp.ts
+++ src/handlers/calentamiento/createOneDayWarmUp.ts
@@ -2,14 +2,16 @@ import { WarmUp } from "../../db";
 import { createOneDayCalentamientoProps } from "../../types";
 import { createDay } from "../day/createDay";
 import { createExercises } from "../ejercicio/createExercises";
+import getOneWarmUp from "./getOneWarmUp";
 
 export const createOneDayWarmUp = async (props: createOneDayCalentamientoProps) => {
     const { warmUpId, day } = props;
     const warmUp: any = await WarmUp.findByPk(warmUpId)
-    if (!warmUp) throw new Error("Rutina no encontrada");
+    if (!warmUp) throw new Error("Calentamiento no encontrado");
     const newDay: any = await createDay(day.day);
     const exercises: any = await createExercises(day.exercises)
     await newDay.addExercise(exercises);
     await warmUp.addDay(newDay)
-    return warmUp;
+    const updateWarmUp = await getOneWarmUp(warmUp.id)
+    return updateWarmUp;
 }
