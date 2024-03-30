d4e38e888da7dc3f73e4542f3db0c379ba854222
Franco Itria <francoitria01@gmail.com>
2024-03-30 17:17:40 -0300

    Loads ordered by week

--- src/handlers/rutina/getOneRoutine.ts
+++ src/handlers/rutina/getOneRoutine.ts
@@ -18,12 +18,19 @@ export default async function getOneRoutine(id: string) {
         }
     })
     routine.Days.sort((day1: { numberDay: number; }, day2: { numberDay: number; }) => day1.numberDay - day2.numberDay)
-    routine.Days.forEach((day: { Exercises: { exercise: number; }[]; }) => {
+    routine.Days.forEach((day: { Exercises: { exercise: number, Loads: { week: number }[] }[]; }) => {
         day.Exercises.sort((exercise1, exercise2) => {
             if (exercise1.exercise && exercise2.exercise) return exercise1.exercise - exercise2.exercise
             return 0
         })
+        day.Exercises.forEach((exercise) => {
+            exercise.Loads.sort((load1, load2) => {
+                if (load1.week && load2.week) return load1.week - load2.week
+                return 0
+            })
+        })
     });
+
     if (!routine) throw new Error('No se encuentra la rutina')
     return routine
 }
