2ad71a2cf8edb2d5280830f73612daccea361760
Franco Itria <francoitria01@gmail.com>
2024-03-30 17:17:13 -0300

    Correct algorithm to create days in routine.

--- src/handlers/rutina/createRoutine.ts
+++ src/handlers/rutina/createRoutine.ts
@@ -7,12 +7,12 @@ import { getOneUserId } from "../user/getOneUserId";
 export const createRoutine = async ({ userId, days }: RoutinePropertys) => {
     const user: any = await getOneUserId(userId)
     const newRutina: any = await Routine.create()
-    days.forEach(async (day) => {
+    for (const day of days) {
         const newDay: any = await createDay(day.day)
         const exercises = await createExercises(day.exercises)
         await newDay.addExercise(exercises)
         await newRutina.addDay(newDay);
-    });
+    }
     await user.addRoutine(newRutina)
     return newRutina
 }
