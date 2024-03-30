bc3276bc5b8ab827116f22a6a090d2bd9a663a2b
Franco Itria <francoitria01@gmail.com>
2024-03-30 17:14:48 -0300

    Correct name variable

--- src/controllers/cargas/updateLoads.ts
+++ src/controllers/cargas/updateLoads.ts
@@ -4,8 +4,8 @@ import { request, response } from "../../types";
 
 export default async function updateLoadsController(req: request, res: response) {
     try {
-        const changeCaras = await updateLoads(req.body)
-        res.status(200).json(changeCaras)
+        const changeCargas = await updateLoads(req.body)
+        res.status(200).json(changeCargas)
     } catch (error) {
         console.log(error)
         res.status(404).json({ Error: error.message })
