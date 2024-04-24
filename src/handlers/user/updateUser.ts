879c7079be2fb68e16e0f453e7c86a643a61f7af
Franco Itria <francoitria01@gmail.com>
2024-04-24 18:26:45 -0300

    Delete console log

--- src/handlers/user/updateUser.ts
+++ src/handlers/user/updateUser.ts
@@ -3,8 +3,7 @@ import { changeUserProps } from "../../types";
 import { getOneUserId } from "./getOneUserId";
 
 export const updateUser = async (changes: changeUserProps) => {
-    const { id, admin, login, contactEmergency, dni, age, email, name, pay, phone, surname, user, password, ban, newGymId, photo} = changes
-    // console.log(id, photo)
+    const { id, admin, login, contactEmergency, dni, age, email, name, pay, phone, surname, user, password, ban, newGymId, photo } = changes
     const existUser: any = await getOneUserId(id)
     let confirmPassword = password
     if (password) { confirmPassword = hash(password) }
