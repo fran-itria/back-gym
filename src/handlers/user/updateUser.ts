353eebf9de301e3408d396344f3c3c5f0bc703bb
Franco Itria <francoitria01@gmail.com>
2024-05-24 01:38:55 -0300

    User update corrected

--- src/handlers/user/updateUser.ts
+++ src/handlers/user/updateUser.ts
@@ -6,28 +6,33 @@ import { getOneUserId } from "./getOneUserId";
 export const updateUser = async (changes: changeUserProps) => {
     const { id, admin, login, contactEmergency, dni, age, email, name, pay, phone, surname, user, password, ban, newGymId, photo } = changes
     const existUser: any = await getOneUserId(id)
-    const existGym: any = await getGymId(newGymId)
-    let confirmPassword = password
-    if (password) { confirmPassword = hash(password) }
-    await existUser.update({
-        admin,
-        contactEmergency,
-        dni,
-        age,
-        email,
-        name,
-        pay,
-        phone,
-        surname,
-        user,
-        password: confirmPassword,
-        login,
-        ban,
-        Gym: {
-            name: existGym.name,
-        },
-        GymId: newGymId,
-        photo
-    })
+    if (newGymId) {
+        const existGym: any = await getGymId(newGymId)
+        await existUser.update({
+            Gym: {
+                name: existGym.name,
+            },
+            GymId: newGymId
+        })
+    } else {
+        let confirmPassword = password
+        if (password) { confirmPassword = hash(password) }
+        await existUser.update({
+            admin,
+            contactEmergency,
+            dni,
+            age,
+            email,
+            name,
+            pay,
+            phone,
+            surname,
+            user,
+            password: confirmPassword,
+            login,
+            ban,
+            photo
+        })
+    }
     return existUser
 }
