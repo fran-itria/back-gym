277b9aac0c35d0b4a9f0b6a78891e5d32c7ae0f4
Franco Itria <francoitria01@gmail.com>
2024-05-24 01:00:47 -0300

    Update name of the gym

--- src/handlers/user/updateUser.ts
+++ src/handlers/user/updateUser.ts
@@ -1,10 +1,12 @@
 import hash from "../../services/hash";
 import { changeUserProps } from "../../types";
+import getGymId from "../gym/getGymId";
 import { getOneUserId } from "./getOneUserId";
 
 export const updateUser = async (changes: changeUserProps) => {
     const { id, admin, login, contactEmergency, dni, age, email, name, pay, phone, surname, user, password, ban, newGymId, photo } = changes
     const existUser: any = await getOneUserId(id)
+    const existGym: any = await getGymId(newGymId)
     let confirmPassword = password
     if (password) { confirmPassword = hash(password) }
     await existUser.update({
@@ -21,6 +23,9 @@ export const updateUser = async (changes: changeUserProps) => {
         password: confirmPassword,
         login,
         ban,
+        Gym: {
+            name: existGym.name,
+        },
         GymId: newGymId,
         photo
     })
