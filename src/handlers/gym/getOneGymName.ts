51785e482506c753ad3f4c023887c133a177a78d
Franco Itria <francoitria01@gmail.com>
2024-05-24 02:54:04 -0300

    Include model User

--- src/handlers/gym/getOneGymName.ts
+++ src/handlers/gym/getOneGymName.ts
@@ -1,7 +1,11 @@
-import { Gym } from "../../db";
+import { Gym, User } from "../../db";
 
 export default async function getOneGymName(name: string) {
-    const gym = await Gym.findOne({ where: { name } })
+    const gym = await Gym.findOne({
+        where: { name }, include: [
+            { model: User }
+        ]
+    })
     if (!gym) throw new Error('Gimnasio no encontrado')
     return gym
 }
