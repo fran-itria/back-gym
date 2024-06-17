bc609a74e4d2c42118ebbb87087f13843d36190e
Franco Itria <francoitria01@gmail.com>
2024-06-17 17:14:07 -0300

    condition for open and close

--- src/handlers/gym/updateGym.ts
+++ src/handlers/gym/updateGym.ts
@@ -4,10 +4,13 @@ import getGymId from "./getGymId";
 export default async function updateGym({ id, name, linkMp, amount, limit, time, close, open }: updateGymProps) {
     const gym = await getGymId(id)
     if (!gym) throw new Error('No existe el gym')
-    const length = Number(close.split(':')[0]) - Number(open.split(':')[0])
-    const start = Number(open.split(':')[0])
-    const end = start + 1
-    const range = Array.from({ length }, (_, i) => `${(start + i) < 10 ? `0${start + i}` : start + i}:00 - ${(end + i) < 10 ? `0${end + i}` : end + i}:00`)
+    let range = []
+    if (open && close) {
+        const length = Number(close.split(':')[0]) - Number(open.split(':')[0])
+        const start = Number(open.split(':')[0])
+        const end = start + 1
+        range = Array.from({ length }, (_, i) => `${(start + i) < 10 ? `0${start + i}` : start + i}:00 - ${(end + i) < 10 ? `0${end + i}` : end + i}:00`)
+    }
     await gym.update({
         name,
         linkMp,
