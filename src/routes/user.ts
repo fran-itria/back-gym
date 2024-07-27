18f5e6e93c403b144ff91be82b63c72e81c4e955
Franco Itria <francoitria01@gmail.com>
2024-07-27 00:55:53 -0300

    New routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -8,6 +8,8 @@ import loginUserController from "../controllers/user/loginUser";
 import logoutController from "../controllers/user/logout";
 import changeGymController from "../controllers/user/cahngeGym";
 import getOneUserDniController from "../controllers/user/getOneUserDni";
+import getOneUserEmailController from "../controllers/user/getOneUserEmail";
+import deleteUserController from "../controllers/user/deleteUser";
 
 const user = Router();
 
@@ -15,10 +17,12 @@ user.get("", getUsersController);
 user.get("/forGym/:gymName", getUsersGymController);
 user.get("/getOneUser/:id", getOneUserIdController);
 user.get("/getOneUserByDni/:dni", getOneUserDniController);
+user.get("/getOneUserByEmail/:email", getOneUserEmailController);
 user.post("", createUserController);
 user.put("", updateUserController);
 user.put("/login", loginUserController);
 user.put("/logout", logoutController)
 user.put("/changeGym", changeGymController)
+user.delete("/delete/:id", deleteUserController);
 
 export default user;
