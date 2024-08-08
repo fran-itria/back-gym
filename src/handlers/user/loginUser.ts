838ee871639e96f31771e97c922893be9dd479f4
Franco Itria <francoitria01@gmail.com>
2024-08-08 03:38:25 -0300

    Error ban

--- src/handlers/user/loginUser.ts
+++ src/handlers/user/loginUser.ts
@@ -5,7 +5,7 @@ import bcrypt from "bcrypt";
 export const loginUser = async ({ user, password }: { user: string, password: string }) => {
     const existUser: any = await User.findOne({ where: { user } })
     if (!existUser) throw new Error('Usuario no encontrado')
-    if (existUser.ban) throw new Error('Se encuentra baneado de la aplicación')
+    if (existUser.ban) throw new Error(existUser.ban)
     if (password.includes('$')) {
         const user = await updateUser({ id: existUser.id, login: true })
         return user
