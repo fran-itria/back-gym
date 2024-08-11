255c70bc02dea032beda6fc91ab8cdd906b091ce
fran-itria <francoitria01@gmail.com>
2024-08-11 00:59:16 -0300

    New routes for mails

--- src/routes/mail.ts
+++ src/routes/mail.ts
@@ -3,16 +3,17 @@ import registerMailController from '../controllers/mails/registerMail'
 import resetPasswordMailController from '../controllers/mails/resetPasswordMail'
 import confirmChangeGymMailController from '../controllers/mails/confirmChangeGym'
 import confirmChangeGymUserMailController from '../controllers/mails/confirmChangeGymUser'
+import createRoutineMailController from '../controllers/mails/createRoutine'
+import banUserMailController from '../controllers/mails/banUserMail'
 const mails = Router()
 export const team = 'Pro Active Center'
 
 mails.post("/registro", registerMailController)
-// mails.post("/newRutina",)
+mails.post("/newRoutine", createRoutineMailController)
 mails.post("/resetPassword", resetPasswordMailController)
-// mails.post("/confirmPayment",)
 mails.post("/confirmChangeGym", confirmChangeGymMailController)
 mails.post("/confirmChangeGymUser", confirmChangeGymUserMailController)
-// mails.post("/banUser",)
+mails.post("/banUser", banUserMailController)
 
 
 export default mails
