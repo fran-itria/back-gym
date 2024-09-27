4f7cd318d0c3ffb38ad21421b8cd7b979b762959
fran-itria <francoitria01@gmail.com>
2024-09-27 20:41:33 -0300

    Delete extra condition

--- src/controllers/user/createUser.ts
+++ src/controllers/user/createUser.ts
@@ -6,10 +6,9 @@ export default async function createUserController(req: request, res: response) 
         const newUser = await createUser(req.body)
         if (newUser) res.status(200).json(newUser)
     } catch (error) {
-        if (error.errors[0].type == 'unique violation' && error.errors[0].path == 'user') {
-            res.status(404).json({ Error: "Usuario ya registrado" })
-        } else if (error.errors[0].type == 'unique violation' && error.errors[0].path == 'email') {
-            res.status(404).json({ Error: "Email ya registrado" })
-        } else res.status(404).json({ Error: error })
+        if (error.errors && error.errors[0].type == 'unique violation') {
+            if (error.errors[0].path == 'user') res.status(404).json({ Error: "Usuario ya registrado" })
+            else if (error.errors[0].path == 'email') res.status(404).json({ Error: "Email ya registrado" })
+        } else res.status(404).json({ Error: error.message })
     }
 }
