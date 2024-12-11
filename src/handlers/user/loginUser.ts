4b065b19ade43e4a1de66da3b9afcdfd23cec7b3
fran-itria <francoitria01@gmail.com>
2024-12-11 02:12:42 -0300

    Save id in token and extend expiration

--- src/handlers/user/loginUser.ts
+++ src/handlers/user/loginUser.ts
@@ -1,22 +1,21 @@
 import { SECRET_KEY } from "../../config";
 import { verifyUser } from "../../services/verifyUser";
+import { getOneUserId } from "./getOneUserId";
 import { updateUser } from "./updateUser"
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken"
 
 export const loginUser = async ({ user, password, token }: { user?: string, password?: string, token?: string }) => {
     if (token) {
-        const data: any = jwt.verify(token, SECRET_KEY)
-        const existUser: any = await verifyUser(data.user)
-        if (data.password == existUser.password) {
-            const user = await updateUser({ id: existUser.id, login: true })
-            return user
-        }
+        const data: JwtPayload | string = jwt.verify(token, SECRET_KEY)
+        const existUser: any = await getOneUserId(data['id'])
+        const user = await updateUser({ id: existUser.id, login: true })
+        return user
     } else if (user && password) {
         const existUser = await verifyUser(user)
         if (bcrypt.compareSync(password, existUser.password)) {
             const user = await updateUser({ id: existUser.id, login: true })
-            const token = jwt.sign({ email: existUser.email, user: existUser.user, password: existUser.password }, SECRET_KEY, { expiresIn: '1h' })
+            const token = jwt.sign({ id: existUser.id }, SECRET_KEY, { expiresIn: '3h' })
             return { user, token }
         } else throw new Error('Contraseña incorrecta')
     }
